e2323bf27c5d99179ba3ac6e2e13f0528864b539
Wanijya <wanijyabisen@gmail.com>
2024-09-25 11:24:34 +0530

    Ascending or descending order

--- sql_codes.sql
+++ sql_codes.sql
@@ -1,47 +1,61 @@
-# Having Clause
+
 /*
-	Jo ki Group By se filter hoke aaya hai data us ko filter krne ke liye Having Clause use hota hai
-    Without Group By Having Clause dose't use.
-    
-    SYNTAX
-			SELECT col_name, AGGREGATE(col_name)...
+	Order by clause is used to sort the data in Ascending or descending order, based on one or more columes
+	SYNTAX: 
+			SELECT col_name...
             FROM table_name
             WHERE [condition]
-            GROUP BY col_name
-            HAVING [aggregate_col_name Condition]
-            
-            # [] it means or ('ho bhi skta hai nhi bhi.')
-*/ 
+            ORDER BY col_name ASC/desc
+*/
 
 use db2;
 
-# Find the total sales of each sub-category having total sales more then 1 lakhs
-SELECT `sub-category`, SUM(sales) AS total_sales
-FROM superstore
-GROUP BY `sub-category`
-HAVING total_sales > 100000;
-
-# Find the avg age of employees from each department whose age is more then 30 and avg age is more then 34
-SELECT department, AVG(emp_age) AS avg_emp_age
+# Find the name and department of the employees and short the department in descending order
+SELECT emp_name, department
 FROM employees
-WHERE emp_age > 30
-GROUP BY department
-HAVING avg_emp_age > 34;
+ORDER BY department DESC;
 
-# Find the total salary of each department whose salary is between 70000 - 90000 and total salary is greater than 3 lakhs
-SELECT department, SUM(salary) AS total_salary
-FROM employees 
-WHERE salary BETWEEN 70000 AND 90000
-GROUP BY department
-HAVING total_salary > 300000;
+# Find the employees name and their age, who belong chicago and denver and short their age in descending order
+SELECT emp_name, emp_age
+FROM employees
+WHERE city IN ('chicago' , 'denver')
+ORDER BY emp_age DESC;
 
-# Find the avg profit and count of quantity of each segment from the east and west region having count of quantity greater than 1200
-SELECT segment,
-    AVG(profit) AS avg_profit,
-    COUNT(quantity) AS cnt_qty
+# Find the avg salary of each department and short the avg salary in descending order
+SELECT department, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department
+ORDER BY avg_salary DESC;
+
+# Find the total profit of each sub-category from east and west region and short the total profit in desc order from superstore table
+SELECT `sub-category`, SUM(profit) AS total_profit
 FROM superstore
 WHERE region IN ('east' , 'west')
-GROUP BY segment
-HAVING cnt_qty > 1200;
+GROUP BY `sub-category`
+ORDER BY total_profit DESC;
+
+# Find the avg sales from each category from the city whose name starts with 'A', 'N' and short the avg sales in asc
+SELECT category, AVG(sales) AS avg_sales
+FROM superstore
+WHERE city REGEXP '^[AN]'
+GROUP BY category
+ORDER BY avg_sales ASC;
+# By the use of Like Oprater
+SELECT category, AVG(sales) AS avg_sales
+FROM superstore
+WHERE city LIKE 'A%' OR city LIKE 'N%'
+GROUP BY category
+ORDER BY avg_sales ASC;
+
+# Find the name of states and their sales value from superstore data and order by states in asc order 
+SELECT state, sales
+FROM superstore
+ORDER BY state ASC , sales DESC;
+
+# Find the cusotmer name and their profit from the superstore data and short the customer name in asc and profit in desc
+SELECT `customer name`, profit
+FROM superstore
+ORDER BY `customer name` ASC , profit DESC;
+
 
 
